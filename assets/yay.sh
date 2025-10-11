aa6b8cc179239b4942e93eee199ead4901bd1f8e
whughesiii2187 <whughesiii@pm.me>
2025-10-10 22:43:47 -0500

    Update yay.sh

--- assets/yay.sh
+++ assets/yay.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-cd /tmp
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si --noconfirm
-cd ~
+if ! command -v yay &> /dev/null; then
+  cd /tmp
+  git clone https://aur.archlinux.org/yay-bin.git
+  cd yay-bin
+  makepkg -si --noconfirm
+  cd ~
+else
+  echo -e "${GREEN} Yay is already installed, adding packages!!${NC}"
 
 echo -e "${GREEN}Installing AUR packages with yay...${NC}"
 yay -Syu --noconfirm
