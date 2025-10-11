b55df74799155e0b9c42d458368597b6b92fe0c2
whughesiii2187 <whughesiii@pm.me>
2025-10-10 23:27:21 -0500

    Update fonts.lua

--- shared/.config/wezterm/config/fonts.lua
+++ shared/.config/wezterm/config/fonts.lua
@@ -2,7 +2,7 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 -- local font_family = 'Maple Mono NF'
-local font_family = 'JetBrainsMono Nerd Font'
+local font_family = '0xProto Nerd Font'
 -- local font_family = 'CartographCF Nerd Font'
 
 local font_size = platform.is_mac and 12 or 9.75
