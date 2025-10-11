45b6e8b0b7ade1fb8cd1734e57d6b674a9bc3a16
whughesiii2187 <whughesiii@pm.me>
2025-10-10 22:38:07 -0500

    Update linux.sh

--- assets/linux.sh
+++ assets/linux.sh
@@ -44,7 +44,7 @@ done
 
 curl -L https://github.com/dundee/gdu/releases/latest/download/gdu_linux_amd64.tgz | tar xz
 chmod +x gdu_linux_amd64
-mv gdu_linux_amd64 /usr/bin/gdu
+sudo mv gdu_linux_amd64 /usr/bin/gdu
 
 # Hyprland Settings App
 #ml4w_app="com.ml4w.hyprlandsettings"
