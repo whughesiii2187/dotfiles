993a883453fe94cbd4a723f2bc7c1ee8368a85c1
whughesiii2187 <whughesiii@pm.me>
2025-10-10 22:40:03 -0500

    Update linux.sh

--- assets/linux.sh
+++ assets/linux.sh
@@ -42,10 +42,6 @@ for pkg in lazygit tree-sitter-cli fzf fd; do
   npm install -g "$pkg"
 done
 
-curl -L https://github.com/dundee/gdu/releases/latest/download/gdu_linux_amd64.tgz | tar xz
-chmod +x gdu_linux_amd64
-sudo mv gdu_linux_amd64 /usr/bin/gdu
-
 # Hyprland Settings App
 #ml4w_app="com.ml4w.hyprlandsettings"
 #ml4w_app_repo="hyprland-settings"
