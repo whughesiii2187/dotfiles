a01a1e368429622529e9715c998c8d70180becea
Bill Hughes <whughesiii@pm.me>
2025-11-06 17:57:39 -0600

    added beervim my attempt at only using mini

---
+++ dotfiles/shared/.config/beervim/lua/plugins/mini.lua
@@ -0,0 +1,122 @@
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = { 'git', 'clone', '--filter=blob:none', 'https://github.com/nvim-mini/mini.nvim', mini_path }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+require('mini.deps').setup({ path = { package = path_package } })
+
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+-- Safely execute immediately
+now(function() require('mini.notify').setup({
+        lsp_progress = { duration_last = 3000,} })
+ end)
+now(function() require('mini.files').setup() end)
+now(function() require('mini.basics').setup() end)
+now(function() require('mini.icons').setup() end)
+now(function() require('mini.tabline').setup() end)
+now(function() require('mini.statusline').setup() end)
+now(function() require('mini.starter').setup() end)
+
+-- Safely execute later
+-- later(function() require('mini.ai').setup() end)
+later(function() require('mini.align').setup() end)
+later(function() require('mini.comment').setup() end)
+later(function() require('mini.completion').setup() end)
+later(function() require('mini.extra').setup() end)
+later(function() require('mini.pick').setup() end)
+later(function() require('mini.pairs').setup() end)
+later(function() require('mini.surround').setup() end)
+later(function() require('mini.snippets').setup() end)
+
+now(function()
+  -- Use other plugins with `add()`. It ensures plugin is available in current
+  -- session (installs if absent)
+  add(
+      {
+        source = 'neovim/nvim-lspconfig',
+        -- Supply dependencies near target plugin
+        depends = { 'williamboman/mason.nvim' },
+      }
+  )
+end)
+now(function()
+  add(
+    {
+      source = "oonamo/ef-themes.nvim"
+    }
+  )
+    require("ef-themes").setup({ 
+      transparent = true 
+    })
+    vim.cmd("colorscheme ef-deuteranopia-dark")
+end)
+
+later(function()
+  add({
+    source = 'nvim-treesitter/nvim-treesitter',
+    -- Use 'master' while monitoring updates in 'main'
+    checkout = 'master',
+    monitor = 'main',
+    -- Perform action after every checkout
+    hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+  })
+  -- Possible to immediately execute code which depends on the added plugin
+  require('nvim-treesitter.configs').setup({
+    ensure_installed = { 'lua', 'vimdoc' },
+    highlight = { enable = true },
+  })
+end)
+
+-- mini clue needed some space
+later(function()
+  local miniclue = require('mini.clue')
+  miniclue.setup({
+    triggers = {
+      -- Leader triggers
+      { mode = 'n', keys = '<Leader>' },
+      { mode = 'x', keys = '<Leader>' },
+
+      -- Built-in completion
+      { mode = 'i', keys = '<C-x>' },
+
+      -- `g` key
+      { mode = 'n', keys = 'g' },
+      { mode = 'x', keys = 'g' },
+
+      -- Marks
+      { mode = 'n', keys = "'" },
+      { mode = 'n', keys = '`' },
+      { mode = 'x', keys = "'" },
+      { mode = 'x', keys = '`' },
+
+      -- Registers
+      { mode = 'n', keys = '"' },
+      { mode = 'x', keys = '"' },
+      { mode = 'i', keys = '<C-r>' },
+      { mode = 'c', keys = '<C-r>' },
+
+      -- Window commands
+      { mode = 'n', keys = '<C-w>' },
+
+      -- `z` key
+      { mode = 'n', keys = 'z' },
+      { mode = 'x', keys = 'z' },
+    },
+
+    clues = {
+      -- Enhance this by adding descriptions for <Leader> mapping groups
+      miniclue.gen_clues.builtin_completion(),
+      miniclue.gen_clues.g(),
+      miniclue.gen_clues.marks(),
+      miniclue.gen_clues.registers(),
+      miniclue.gen_clues.windows(),
+      miniclue.gen_clues.z(),
+    },
+  }) 
+end)
