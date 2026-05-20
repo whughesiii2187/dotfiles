e730998cf0db491a7e91153829e085cfe644a25c
Bill Hughes <whughesiii@pm.me>
2026-05-19 20:52:08 -0500

    let's try it

--- devc/setup.sh
+++ devc/setup.sh
@@ -6,14 +6,18 @@ XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 echo "Installing Brew"
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 
-echo "Installing oh-my-zsh"
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
-
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 brew update
 
 echo "Installing packages"
+brew install zsh
+echo "/home/linuxbrew/.linuxbrew/bin/zsh" | sudo tee -a /etc/shells
+chsh -s /home/linuxbrew/.linuxbrew/bin/zsh "$USER"
+
+echo "Installing oh-my-zsh"
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+
 brew install neovim fzf lazygit zsh tmux copilot claude-code font-0xproto-nerd-font gcc clipboard ripgrep
 
 ### ----------------------------
