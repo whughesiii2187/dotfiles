9c08afe10df0a7d8190f0c0a9027f4c7a8dd2d78
Bill Hughes <whughesiii@pm.me>
2026-09-19 21:46:23 -0500

    fix mason reference

--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -2,7 +2,12 @@
 -- LazyVim lang.go extra (which pulls in dap-go, gotests, etc.) is enabled.
 return {
   {
-    "mason-org/mason.nvim",
+    -- bare name: extends whichever mason.nvim source LazyVim core is
+    -- actually pinned to, instead of forcing an owner that may not match
+    -- (mason.nvim moved from williamboman/ to mason-org/ upstream; pinning
+    -- a specific owner here caused a source mismatch with LazyVim core and
+    -- broke ensure_installed entirely)
+    "mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "gopls",
